730ba0dba090b3e3d7610aa824f6e232d009320c
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-01-19 16:00:49 -0600

    add in template form

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import './style.css'
+import Task from './createTaskClass.js';
 
-let testArray = [1,2,'test']
-let testObject = {test1: 'test-data'}
-localStorage.setItem('test1', 'test1-data')
-localStorage.setItem("test2", "test2-data")
-localStorage.setItem("test3", testObject)
+const todoSubmitButton = document.getElementById('todo-submit');
+const taskForm = document.forms['task-form']
 
-let testrecall = localStorage.getItem('test3')
-console.table(testrecall)
+let tasks = [];
+
+let testTast = new Task ('testTitle', 'testDescription', 'testdate', 'urgent');
+tasks.push(testTast)
+testTast = new Task ('testtitloe2', 'testderasdfg', 'date test', 'not urgent')
+tasks.push(testTast)
+console.log(tasks)
+
+todoSubmitButton.addEventListener('click', (clickEvent) => {
+    clickEvent.preventDefault();
+    console.log(taskForm.elements['description'].value)
+    taskForm.reset()
+})
+
